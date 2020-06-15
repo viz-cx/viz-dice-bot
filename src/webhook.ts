621bd224cbe42ada885392fc25b1dd75ae474ab8
Vladimir Babin <vovababin@gmail.com>
2020-06-15 23:16:15 +0500

    Add setWebhook

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,6 +1,12 @@
 import bot from './bot'
 
+const domain = process.env.DOMAIN || ''
+if (!domain) {
+    console.error('You should set DOMAIN environment with / at the end')
+}
 const path = process.env.SECRET_PATH || '/secret-path'
 const port = parseInt(process.env.PORT) || 8080
+
+bot.telegram.setWebhook(domain + path)
 bot.startWebhook(path, null, port)
 console.info('Bot is up and running on port', port)
