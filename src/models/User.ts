a9f75990546f8be0ced2ea34b0abf1f9152faf09
Vladimir Babin <vovababin@gmail.com>
2020-06-15 14:26:55 +0500

    Add encryption for private keys

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
-import { prop, getModelForClass } from '@typegoose/typegoose'
+import { prop, getModelForClass, pre } from '@typegoose/typegoose'
 import { DiceEmoji } from 'telegraf/typings/telegram-types'
+import CryptoJS = require('crypto-js')
 
 export type UserState =
   'empty'
@@ -25,10 +26,15 @@ export class User {
   @prop({ required: true, default: 1 })
   series: number
 
-  @prop({ required: false, unique: true })
+  @prop({ required: false, unique: true, trim: true })
   login: string
 
-  @prop({ required: false, unique: false })
+  @prop({
+    required: false, unique: false,
+    // encryption in case of leaking database
+    set: (val: string) => val.length == 0 ? '' : CryptoJS.AES.encrypt(val, process.env.SECRET).toString(),
+    get: (val: string) => val.length == 0 ? '' : CryptoJS.AES.decrypt(val, process.env.SECRET).toString(CryptoJS.enc.Utf8)
+  })
   postingKey: string
 }
 
