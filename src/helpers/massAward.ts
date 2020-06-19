d075cb20cc19d05f412ca73d2ef77ecaf5845f39
Vladimir Babin <vovababin@gmail.com>
2020-06-19 18:58:06 +0500

    Get mass award energy from account energy

--- src/helpers/massAward.ts
+++ src/helpers/massAward.ts
@@ -13,11 +13,14 @@ export function setupMassAward() {
                     .filter(user => user.login && user.postingKey)
                     .map(user => user.login)
                 const energyRestoreDays = 5
-                const energy = Math.trunc(10000 / energyRestoreDays / 24 * hoursInterval)
-                viz.massAward(process.env.ACCOUNT, process.env.WIF, '🎲 🎯 🏀', energy, logins).then(
-                    _ => console.log('Successfully mass awarded', logins.length, 'accounts'),
-                    err => console.log('Mass award failed:', err)
-                )
+                viz.getAccountEnergy(process.env.ACCOUNT)
+                    .then(accountEnergy => {
+                        const energy = Math.trunc(accountEnergy / 5)
+                        viz.massAward(process.env.ACCOUNT, process.env.WIF, '🎲 🎯 🏀', energy, logins).then(
+                            _ => console.log('Successfully mass awarded', logins.length, 'accounts'),
+                            err => console.log('Mass award failed:', err)
+                        )
+                    }, err => console.log('Account energy error', err))
             },
             err => console.log('Mass award error:', err)
         )
