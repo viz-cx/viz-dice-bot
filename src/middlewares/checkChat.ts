91eeae463fd39cb94bc60e883b17d97dbfe629ec
Vladimir Babin <vovababin@gmail.com>
2021-03-09 14:32:37 +0500

    Update chat type condition

--- src/middlewares/checkChat.ts
+++ src/middlewares/checkChat.ts
@@ -1,6 +1,7 @@
 import { Context } from 'telegraf'
 
 export async function checkChatType(ctx: Context, next: () => any) {
-    console.log(ctx.chat)
-    next()
+    if (ctx.chat !== undefined || ctx.chat.type === 'private') {
+        next()
+    }
 }
