04f43a1eadf00da0c6653d90baa3f4c9d1c4806e
Vladimir Babin <vovababin@gmail.com>
2021-05-10 17:54:50 +0500

    Check context property

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -5,8 +5,12 @@ import { Context } from 'telegraf'
 const viz = new VIZ()
 
 export async function attachUser(ctx: Context, next) {
-  const dbuser = await getOrCreateUser(ctx.from.id)
-  ctx.dbuser = dbuser
-  ctx.viz = viz
-  next()
+  if (ctx.from) {
+    const dbuser = await getOrCreateUser(ctx.from.id)
+    ctx.dbuser = dbuser
+    ctx.viz = viz
+    next()
+  } else {
+    console.log("Context from empty:", ctx)
+  }
 }
